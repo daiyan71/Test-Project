e10ad760bc8cc124048437a50bfa1e0f8c2e44f2
daiyan71 <daiyanibrahim12@gmail.com>
2022-04-21 13:45:49 +0600

    added marking

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   resources :courses
   resources :semesters
   get 'admin', to: "admin_home#home"
+  get 'update_mark/:id', to: "admin_home#updateMark" , as: "update_mark"
   get 'specificCourses/:id', to: "courses#indexSpecific" ,  as: "specific_course"
   get 'newSpecificCourse/:id', to: "courses#newSpecific" ,  as: "new_specific_course"
   get 'selectCurrentSemester/:id', to: "admin_home#selectCurrentSemester" ,  as: "select_current_sem"
   get 'admin/students', to: "admin_home#allStudent" ,  as: "students"
+  
   devise_for :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'see_courses', to: "student_home#seeCourses" ,  as: "see_courses"

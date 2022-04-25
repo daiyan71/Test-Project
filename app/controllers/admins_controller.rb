d16b035b2e74ab32698f9c15b3d1113d43e54b2a
daiyan71 <daiyanibrahim12@gmail.com>
2022-04-25 14:46:21 +0600

    validation added

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -26,7 +26,7 @@ class AdminsController < ApplicationController
 
     respond_to do |format|
       if @admin.save
-        format.html { redirect_to admin_url(@admin), notice: "Admin was successfully created." }
+        format.html { redirect_to admins_url, notice: "Admin was successfully created." }
         format.json { render :show, status: :created, location: @admin }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class AdminsController < ApplicationController
   def update
     respond_to do |format|
       if @admin.update(admin_params)
-        format.html { redirect_to admin_url(@admin), notice: "Admin was successfully updated." }
+        format.html { redirect_to admins_url, notice: "Admin was successfully updated." }
         format.json { render :show, status: :ok, location: @admin }
       else
         format.html { render :edit, status: :unprocessable_entity }
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   after_destroy :ensure_an_admin_remains
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email,:with => Devise::email_regexp
+
   has_secure_password
 
   class Error < StandardError
